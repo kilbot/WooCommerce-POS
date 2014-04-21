041fdd77d919ecb9bfa0b8d566c66c8ca7b22292
Paul Kilmurray <paul@kilbot.com.au>
2014-04-21 17:42:10 +0800

    GitHub Updater

--- woocommerce-pos.php
+++ woocommerce-pos.php
@@ -17,7 +17,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/kilbot/WooCommerce-POS
+ * GitHub Plugin URI: https://github.com/kilbot/woocommerce-pos
  * GitHub Branch:     master
  */
 
