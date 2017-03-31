6e8eac23d63fc0974b9d57b9c60862763e8210d6
Paul Kilmurray <paul@kilbot.com.au>
2017-03-31 22:03:45 +0800

    Fix travis sed

--- tests/unit/php/bootstrap.php
+++ tests/unit/php/bootstrap.php
@@ -14,6 +14,11 @@ class Unit_Test_WC_POS {
     require_once $this->wp_tests_dir . '/includes/functions.php';
     tests_add_filter( 'muplugins_loaded', array( $this, 'load_wc_pos' ) );
     require_once $this->wp_tests_dir . '/includes/bootstrap.php';
+
+    // disable depreciation notices for WC 3+
+    if( version_compare( WC()->version, '3', '>=' ) ){
+      add_filter('doing_it_wrong_trigger_error', '__return_false');
+    }
   }
 
   public function load_wc_pos() {
