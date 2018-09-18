91fbfb155257b80c5dbd3fc97d3b9df5ec0587cb
Paul Kilmurray <paul@kilbot.com.au>
2018-09-19 04:48:28 +0530

    update docker

--- tests/unit/php/bootstrap.php
+++ tests/unit/php/bootstrap.php
@@ -11,7 +11,16 @@ class Unit_Tests {
     ini_set( 'display_errors','on' );
     error_reporting( E_ALL );
 
-    $this->wp_tests_dir = '/var/www/html/wordpress-tests-lib';
+    $this->wp_tests_dir = '/var/www/html/wp/latest/wordpress-tests-lib'; // default folder
+    $lines = file('/var/www/html/wp-cli.yml', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $matches = preg_grep("/^path:(.*?)$/", $lines);
+    if($matches) {
+      $this->wp_tests_dir = str_replace(
+        'wp/latest',
+        trim(str_replace('path:','',$matches[0])),
+        $this->wp_tests_dir
+      );
+    }
 
     require_once $this->wp_tests_dir . '/includes/functions.php';
     tests_add_filter( 'muplugins_loaded', array( $this, 'load' ) );
@@ -20,8 +29,8 @@ class Unit_Tests {
 
   public function load() {
 //    require_once WP_PLUGIN_DIR . '/woocommerce-pos/vendor/autoload.php';
-    require_once WP_PLUGIN_DIR . '/woocommerce/woocommerce.php';
-    require_once WP_PLUGIN_DIR . '/woocommerce-pos/woocommerce-pos.php';
+    require_once '/var/www/html/plugins/woocommerce/woocommerce.php';
+    require_once '/var/www/html/plugins/woocommerce-pos/woocommerce-pos.php';
 
     WC()->api->includes();
   }
